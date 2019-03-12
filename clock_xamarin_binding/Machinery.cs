9b82d3bf3271300985931824dad95a6488d33f91
Guiweeneth <ana.arrme@gmail.com>
2019-03-12 21:36:08 +0100

    Primer commit

---
+++ clock_xamarin_binding/Machinery.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Timers;
+namespace clock_xamarin_binding
+{
+    public class Machinery : NotificationObject
+    {
+        private int hour;
+
+        public int Hour
+        {
+            get { return hour; }
+            set
+            {
+                hour = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private int minute;
+
+        public int Minute
+        {
+            get { return minute; }
+            set
+            {
+                minute = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private int second;
+
+        public int Second
+        {
+            get { return second; }
+            set
+            {
+                second = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public Machinery()
+        {
+            var time = new Timer();
+            time.Elapsed += HandleTime;
+            time.AutoReset = true;
+            time.Enabled = true;
+        }
+
+        void HandleTime(object sender, ElapsedEventArgs e)
+        {
+            Hour = e.SignalTime.Hour;
+            Minute = e.SignalTime.Minute;
+            Second = e.SignalTime.Second;
+        }
+
+    }
+}
---
+++ clock_xamarin_binding/NotificationObject.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace clock_xamarin_binding
+{
+    public abstract class NotificationObject : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
